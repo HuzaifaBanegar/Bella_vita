76bf72536f1fea6cedfcb0b22dae7e00ff4eada6
HuzaifaBanegar <banegarhuzzi1010@gmail.com>
2022-02-23 00:41:16 +0530

    Navbar and footer uploaded

---
+++ Navbar and bottom panel/components/navbar.js
@@ -0,0 +1,44 @@
+const navbar = () => {
+    return `
+    <div id="headnav">
+        <img src="./images/Screenshot (110).png">
+    </div>
+    <div id="navmobile">
+        <div class="navmobfirst">
+            <img src="./images/743930_button_512x512.png">
+            <img src="./images/magnifying-glass-icon-13.png">
+        </div>
+        <div><img src="./images/Screenshot (109).png"></div>
+        <div class="navmoblast">
+            <img src="./images/OIP.jpg">
+            <img src="./images/img_290616.png">
+        </div>
+    </div>
+    <div id="huznav">
+        <a><img src="./images/Screenshot (109).png"></a>
+        <div class="huz_content">
+            <p>ALL PRODUCTS</p>
+            <p>BESTSELLERS</p>
+            <p>OFFERS</p>
+            <p>SELFIE SWITCH</p>
+            <p>BUILD A BOX</p>
+            <p>BY CATEGORY</p>
+            <p>BY CONTENT</p>
+            <p>BY CONCERN</p>
+            <p>BY INGREDIENTS</p>
+            <p>BY COMBOS</p>
+            <p>OUR CELEBRITIES</p>
+            <p>NATURAL SOAP</p>
+            <p>MORE</p>
+        </div>
+        <div class="navicons">
+            <img src="./images/magnifying-glass-icon-13.png"/>
+            <img src="./images/OIP.jpg">
+            <img src="./images/img_290616.png">
+        </div>
+    </div>`
+}
+export default navbar;
+
+
+    
